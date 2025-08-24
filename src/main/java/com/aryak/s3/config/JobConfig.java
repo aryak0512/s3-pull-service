f44a371da5d334fbbe57843c4f7b704e9ecc80fe
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-08-24 18:09:42 +0530

    Job trigger code added (but breaking)

--- src/main/java/com/aryak/s3/config/JobConfig.java
+++ src/main/java/com/aryak/s3/config/JobConfig.java
@@ -1,21 +1,75 @@
 package com.aryak.s3.config;
 
+import com.aryak.s3.model.MyRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.transaction.PlatformTransactionManager;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
+@Configuration
 public class JobConfig {
 
-//    @Bean
-//    @StepScope
-//    public FlatFileItemReader<MyRecord> reader(
-//            @Value("#{jobParameters['bucket']}") String bucket,
-//            @Value("#{jobParameters['key']}") String key,
-//            AmazonS3 amazonS3) {
-//        S3Object s3Object = amazonS3.getObject(bucket, key);
-//
-//        return new FlatFileItemReaderBuilder<MyRecord>()
-//                .name("s3FileReader")
-//                .resource(new InputStreamResource(s3Object.getObjectContent()))
-//                .delimited()
-//                .names("col1", "col2", "col3")
-//                .targetType(MyRecord.class)
-//                .build();
-//    }
+    private static final Logger log = LoggerFactory.getLogger(JobConfig.class);
+    private final S3Client s3Client;
+
+    public JobConfig(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    @Bean
+    public Job firstJob(JobRepository jobRepository, Step step1) {
+        return new JobBuilder("job1", jobRepository)
+                .start(step1)
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository,
+                      PlatformTransactionManager transactionManager,
+                      FlatFileItemReader<MyRecord> reader) {
+        return new StepBuilder("step1", jobRepository)
+                .<MyRecord, MyRecord>chunk(100, transactionManager)
+                .reader(reader)
+                .writer(it -> System.out.println("hi"))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<MyRecord> reader(
+            @Value("#{jobParameters['bucket']}") String bucket,
+            @Value("#{jobParameters['key']}") String key) {
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        ResponseInputStream<GetObjectResponse> s3Object = s3Client.getObject(getObjectRequest);
+        return new FlatFileItemReaderBuilder<MyRecord>()
+                .name("s3FileReader")
+                .resource(new InputStreamResource(s3Client.getObject(getObjectRequest, ResponseTransformer.toInputStream())))
+                .delimited()
+                .names("id", "name", "city")
+                .targetType(MyRecord.class)
+                .build();
+    }
+
+
 }
--- src/main/java/com/aryak/s3/listeners/S3FileListener.java
+++ src/main/java/com/aryak/s3/listeners/S3FileListener.java
@@ -1,8 +1,13 @@
-package com.aryak.s3.config;
+package com.aryak.s3.listeners;
 
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.eventnotifications.s3.model.S3EventNotification;
 import software.amazon.awssdk.eventnotifications.s3.model.S3EventNotificationRecord;
@@ -13,11 +18,15 @@ import java.util.List;
 public class S3FileListener {
 
     private static final Logger log = LoggerFactory.getLogger(S3FileListener.class);
+    @Autowired
+    JobLauncher jobLauncher;
+    @Autowired
+    Job firstJob;
 
     @SqsListener("${sqs.queue-name}")
     public void handleS3Event(String messageJson) {
         try {
-            // Try parsing the incoming JSON as S3 event
+
             S3EventNotification event = S3EventNotification.fromJson(messageJson);
             List<S3EventNotificationRecord> records = event.getRecords();
 
@@ -27,15 +36,21 @@ public class S3FileListener {
             }
 
             for ( S3EventNotificationRecord record : records ) {
+
                 String bucket = record.getS3().getBucket().getName();
                 String key = record.getS3().getObject().getKey();
 
                 log.info("New S3 file detected. Bucket: {}, Key: {}", bucket, key);
-                // TODO: trigger your Spring Batch job here
+
+                JobParameters parameters = new JobParametersBuilder()
+                        .addJobParameter("bucket", bucket, String.class)
+                        .addJobParameter("key", key, String.class)
+                        .toJobParameters();
+
+                jobLauncher.run(firstJob, parameters);
             }
 
         } catch (Exception e) {
-            // Catch-all safeguard
             log.error("Unexpected error while processing S3 event: {}", messageJson, e);
         }
 
